2dce5e24519a070371084fe146e9cd19eddc187c
adrilafosse <81637698+adrilafosse@users.noreply.github.com>
2023-02-09 13:07:16 +0100

    o

--- app/src/main/java/com/example/exercice1/CustomAdapter.kt
+++ app/src/main/java/com/example/exercice1/CustomAdapter.kt
@@ -1,4 +1,5 @@
 package com.example.exercice1
+import android.provider.MediaStore.Images.Thumbnails.getThumbnail
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -6,12 +7,16 @@ import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.exercice1.databinding.CellCustomBinding
+import com.squareup.picasso.Picasso
+import com.example.exercice1.network.Plate
 
 class CustomAdapter(val items: List<String>, val clickListener:(Int) -> Unit): RecyclerView.Adapter<CustomAdapter.CellViewHolder>() {
     class CellViewHolder(binding: CellCustomBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        val textView: TextView=binding.textView
+        val textView: TextView = binding.itemName
         val root : ConstraintLayout = binding.root
+        val imageView = binding.imageView
+        val priceTextView = binding.priceTextView
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CellViewHolder {
@@ -24,11 +29,21 @@ class CustomAdapter(val items: List<String>, val clickListener:(Int) -> Unit): R
     }
 
     override fun onBindViewHolder(holder: CellViewHolder, position: Int) {
-
-        holder.textView.text = items[position]
+        val plate = items[position]
+        holder.textView.text = plate.name
+        holder.priceTextView.text = plate.prices.first().price + " €"
+        Picasso.get().load(getThumbnail(plate)).into(holder.imageView)
         holder.root.setOnClickListener {
-            Log.d("item", "Click on"+position+" position")
-            clickListener(position)
+            Log.d("click", "click on ${position}")
+            clickListener(plate)
+        }
+    }
+
+    private fun getThumbnail(plate: Plate): String? {
+        return if (plate.images.isNotEmpty() && plate.images.firstOrNull()?.isNotEmpty() == true) {
+            plate.images.firstOrNull()
+        } else {
+            null
         }
     }
 }
--- app/src/main/java/com/example/exercice1/MenuListActivity.kt
+++ app/src/main/java/com/example/exercice1/MenuListActivity.kt
@@ -1,10 +1,18 @@
 package com.example.exercice1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request.Method
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.exercice1.databinding.ActivityMenuListBinding
+import com.google.gson.GsonBuilder
+import org.json.JSONObject
+import com.example.exercice1.network.NetworkConstants
+import com.example.exercice1.network.MenuResult
 
 
 enum class Category { STARTER, MAIN, DESSERT }
@@ -16,27 +24,34 @@ class MenuListActivity : AppCompatActivity() {
     }
 
     lateinit var binding: ActivityMenuListBinding
+    lateinit var currentCategory: Category
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMenuListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val category = intent.getSerializableExtra(extraKey) as? Category
-        supportActionBar?.title = categoryName( category ?: Category.STARTER)
-        showData()
-        // if category == null { category = STARTER }
+        currentCategory = category ?: Category.STARTER
+        supportActionBar?.title = categoryName()
+
+        makeRequest()
     }
-    private fun categoryName(category: Category): String {
-        return when(category) {
+    private fun categoryName(): String {
+        return when(currentCategory) {
             Category.STARTER -> getString(R.string.starter)
             Category.MAIN -> getString(R.string.main)
             Category.DESSERT -> getString(R.string.finish)
         }
     }
 
-    private fun showData(){
-        binding.recyclerView.layoutManager=LinearLayoutManager( this)
-        binding.recyclerView.adapter=CustomAdapter(listOf("1","2","3")) { position ->}
+    private fun showDatas(category: com.example.exercice1.network.Category) {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = CustomAdapter(category.items) {
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra(DetailActivity.PLATE_EXTRA, it)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
@@ -56,5 +71,40 @@ class MenuListActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         Log.d("lifeCycle", "MainActivity onPause")
+    }
+
+    private fun parseData(data: String) {
+        val result = GsonBuilder().create().fromJson(data, MenuResult::class.java)
+        val category = result.data.first { it.name == categoryFilterKey() }
+        showDatas(category)
+    }
+
+    private fun makeRequest() {
+        val queue = Volley.newRequestQueue(this)
+        val params = JSONObject()
+        params.put(NetworkConstants.idShopKey, 1)
+        val request = JsonObjectRequest(
+            Method.POST,
+            NetworkConstants.url,
+            params,
+            { result ->
+                // Success of request
+                Log.d("request", result.toString(2))
+                parseData(result.toString())
+            },
+            { error ->
+                // Error when request
+                Log.e("request", error.toString())
+            }
+        )
+        queue.add(request)
+        //showDatas()
+    }
+    private fun categoryFilterKey(): String {
+        return when(currentCategory) {
+            Category.STARTER -> "Entrées"
+            Category.MAIN -> "Plats"
+            Category.DESSERT -> "Desserts"
+        }
     }
 }
